7e25cf09822ca22e3fbc2f2b50c9f633d2172928
Connor <connor_ford1@baylor.edu>
2023-02-07 14:50:29 -0600

    After Class 2/6

--- 7_schools_report.py
+++ 7_schools_report.py
@@ -11,9 +11,31 @@ Copy that info here:
 "NCAA/NAIA conference number football (IC2020)","130","Southeastern Conference"
 
 
-Display report for all universities that have a graduation rate for Women over 50%
+Display report for all universities that have a graduation rate for Women over 75%
 Display report for all universities that have a total price for in-state students living off campus over $50,000
 
 
 
 """
+import json
+
+infile = open('school_data.json', 'r')
+
+schools = json.load(infile)
+
+print(type(schools))
+
+conference_schools = [372, 108, 107, 130]
+
+#How many schools are in this file
+print(len(schools))
+
+for x in schools:
+    #print(x["Graduation rate  women (DRVGR2020)"])
+    if x["Graduation rate  women (DRVGR2020)"]:
+        if x["Graduation rate  women (DRVGR2020)"] > 75:
+            print(x)
+            print()
+
+for x in schools:
+    
